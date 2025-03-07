27d63f9d075b2a016d96d76dba2c1e4f0ae84c31
Nazmul Huda <nznazmulhuda04@gmail.com>
2025-03-07 23:00:01 +0600

    product card added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,23 @@
 import Banner from "@/components/banner/banner"
+import ProductCardSmall from "@/components/cards/product/small"
 
 function Homepage() {
   return (
     <main>
       {/* banner section */}
-      <section className="h-[720px] flex items-center justify-center">
+      {/* <section className="h-[720px] flex items-center justify-center">
         <Banner />
+      </section> */}
+
+      {/* products section */}
+      <section>
+        <main className="container mx-auto grid grid-cols-4 gap-8">
+          {/* product cards */}
+          <ProductCardSmall />
+          <ProductCardSmall />
+          <ProductCardSmall />
+          <ProductCardSmall />
+        </main>
       </section>
     </main>
   )
---
+++ src/components/cards/product/small.tsx
@@ -0,0 +1,74 @@
+import {
+  Card,
+  CardContent,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+  CardDescription
+} from "@/components/ui/card"
+import { Separator } from "@/components/ui/separator"
+import Image from "next/image"
+import { AddToCartButton, WishlistButton } from "./buttons"
+
+
+function ProductCardSmall() {
+  return (
+    <Card className="border border-[#F1F1F1] rounded-[8px] bg-white p-4 gap-3">
+      <CardHeader className="p-0 relative">
+        {/* product label */}
+        <CardTitle className="absolute top-5 left-4">
+          <button className="border border-[#E2E3E6] bg-[#F1F1F1] text-black px-1.5 py-0.5 rounded-[6px] text-[12px] font-normal">
+            Label
+          </button>
+        </CardTitle>
+
+        {/* product image */}
+        <div className="w-full flex items-center justify-center bg-[#F6F6F6] rounded-[4px]">
+          <Image
+            priority
+            alt="Product image"
+            layout="responsive"
+            width={248}
+            height={220}
+            src="https://placehold.co/248x220/webp"
+            className="rounded-[4px]"
+          />
+        </div>
+      </CardHeader>
+
+      <CardContent className="p-0">
+        {/* price */}
+        <h5
+          style={{
+            letterSpacing: -0.4
+          }}
+          className="text-black font-medium text-[14px] leading-[22px] mb-1">
+          $99.00
+        </h5>
+
+        <CardDescription
+          style={{
+            letterSpacing: -0.5
+          }}
+          className="text-[#707378] font-medium text-[16px] leading-[24px]"
+        >
+          Product name
+        </CardDescription>
+      </CardContent>
+
+      <Separator className="bg-[#E2E3E6]" />
+
+
+      <CardFooter className="p-0 space-x-2">
+        {/* wishlist button */}
+        <WishlistButton />
+
+        {/* add to cart button */}
+        <AddToCartButton />
+      </CardFooter>
+    </Card>
+
+  )
+}
+
+export default ProductCardSmall
